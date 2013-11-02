161175b36dbb88ff74c5707b4dfe8d61039fb740
F1tZ81 <f1tz81@gmail.com>
2013-11-02 08:33:03 -0400

    3.0 Luncher Project

---
+++ DSMinecraft/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSMinecraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process Minecraft = new Process();
+            Minecraft.StartInfo.UseShellExecute = false;
+            Minecraft.StartInfo.EnvironmentVariables.Remove("APPDATA");
+            Minecraft.StartInfo.EnvironmentVariables.Add("APPDATA", System.IO.Directory.GetCurrentDirectory() + "\\data");
+            Minecraft.StartInfo.FileName = "bin\\minecraft.exe";
+            Minecraft.Start();
+        }
+    }
+}
