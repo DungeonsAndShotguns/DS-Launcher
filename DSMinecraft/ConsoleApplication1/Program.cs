f0cd2b6197376a70f1c59f9b273cf3eb7c1ed8b3
F1tZ81 <f1tz81@gmail.com>
2013-11-08 12:20:46 -0500

    Added back in .net auto update, will convert to new CPTSpaceToster format. !!! This will be replaced by the Java version for cross compatibility with other operating systems.

--- DSMinecraft/ConsoleApplication1/Program.cs
+++ DSMinecraft/ConsoleApplication1/Program.cs
@@ -1,22 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MinecraftHelper;
 
 namespace DSMinecraft
 {
     class Program
     {
+        public static string CurrentVersion = string.Empty;
+        public static DataTable Releases = new DataTable();
+        public static string VersionFileURL = null;
+
         static void Main(string[] args)
         {
+            // Version Cehcking
+            Releases.Columns.Add(new DataColumn("ReleaseNumber", typeof(string)));
+            Releases.Columns.Add(new DataColumn("DownloadLocation", typeof(string)));
+            Releases.Columns.Add(new DataColumn("ExeToRun", typeof(string)));
+
+            if (CheckVersion(Directory.GetCurrentDirectory()).Contains("Good") == false)
+            {
+                Update update = new Update();
+                //update.Show();
+                System.Windows.Forms.Application.Run(update);
+            }
+
+
             Process Minecraft = new Process();
             Minecraft.StartInfo.UseShellExecute = false;
             Minecraft.StartInfo.EnvironmentVariables.Remove("APPDATA");
             Minecraft.StartInfo.EnvironmentVariables.Add("APPDATA", System.IO.Directory.GetCurrentDirectory() + "\\data");
             Minecraft.StartInfo.FileName = "bin\\minecraft.exe";
             Minecraft.Start();
+        }
+
+        public static string CheckVersion(string VersionFileDir)
+        {
+            string RemoteVersion = string.Empty;
+
+            CurrentVersion = "";
+
+            Releases.Clear();
+
+            string Versions = NetHelper.Download("https://dl.dropboxusercontent.com/u/5921811/update.txt");
+
+            string[] Releasesx = Versions.Split(new string[] { "\n" }, StringSplitOptions.None);
+
+            foreach (string VerItem in Releasesx)
+            {
+                Releases.Rows.Add(VerItem.Split(new char[] { ';' }));
+            }
+
+            int CurrentVersionNum = 0;
+            bool FoundCurrentVersion = false;
+            string LatestVersion = "";
+
+            foreach (DataRow CurrentRow in Releases.Rows)
+            {
+                if (FoundCurrentVersion == false)
+                {
+                    CurrentVersionNum++;
+                }
+                if (CurrentRow["ReleaseNumber"].ToString() == CurrentVersion)
+                {
+                    FoundCurrentVersion = true;
+                    break;
+                }
+                LatestVersion = CurrentRow["ReleaseNumber"].ToString();
+            }
+
+            if (CurrentVersionNum != Releases.Rows.Count)
+            {
+                //return latest version
+                return LatestVersion;
+            }
+            else
+            {
+                return "Version Good: " + LatestVersion;
+            }
         }
     }
 }
