ff537616a3e95712151cbde270eab0ef6495ee54
f1tz81@gmail.com <bn17231981>
2015-01-26 00:08:31 -0500

    DS MMC Quick lunch

---
+++ DSMinecraft/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSMinecraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process Minecraft = new Process();
+            Minecraft.StartInfo.FileName = "MultiMC.exe";
+            Minecraft.Start();
+        }
+    }
+}
